8922ad5f58efda1571188995041b1f141fcf3c9a
gloutch <ismael.herve@yahoo.com>
2021-03-31 23:29:57 +0200

    Improve script

--- tst/run-suite.sh
+++ tst/run-suite.sh
@@ -3,6 +3,7 @@ function usage() {
     echo "$0 <png-plte> <suite folder>"
 }
 
+
 if [ $# -ne 2 ]; then
     usage
     exit 1
@@ -13,19 +14,14 @@ PLTE_EXE=$1
 TST_SUITE=$2
 
 
-echo "$PLTE_EXE --bmp=$TST_SUITE/*.bmp $TST_SUITE/*.png"
-
 for file in $TST_SUITE/*.png
 do
     output=${file/.png/.bmp}
     ./$PLTE_EXE --bmp=$output $file > /dev/null
 
     if [ $? -eq 0 ]; then
-        echo -n "."
-    else
-        echo -n "x"
-    fi    
+        COUNT_BMP=$((COUNT_BMP+1))
+    fi
 done
 
-echo
-echo "Done"
+echo `ls $TST_SUITE/*.bmp | wc -l` / `ls $TST_SUITE/*.png | wc -l`
