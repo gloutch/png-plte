d01d3af748c0e2e1e6b61ce245fe9a92871fbb60
gloutch <ismael.herve@yahoo.com>
2021-04-01 22:51:19 +0200

    improve script

--- tst/run-suite.sh
+++ tst/run-suite.sh
@@ -3,7 +3,6 @@ function usage() {
     echo "$0 <png-plte> <suite folder>"
 }
 
-
 if [ $# -ne 2 ]; then
     usage
     exit 1
@@ -16,12 +15,13 @@ TST_SUITE=$2
 
 for file in $TST_SUITE/*.png
 do
+    # .bmp file with the same name as .png 
     output=${file/.png/.bmp}
     ./$PLTE_EXE --bmp=$output $file > /dev/null
-
-    if [ $? -eq 0 ]; then
-        COUNT_BMP=$((COUNT_BMP+1))
-    fi
 done
 
-echo `ls $TST_SUITE/*.bmp | wc -l` / `ls $TST_SUITE/*.png | wc -l`
+COUNT_BMP=`ls $TST_SUITE/*.bmp | wc -l`
+COUNT_PNG=`ls $TST_SUITE/*.png | wc -l`
+PERCENT=`expr $COUNT_BMP \* 100 / $COUNT_PNG`
+
+echo $PERCENT% "  :  " $COUNT_BMP / $COUNT_PNG
